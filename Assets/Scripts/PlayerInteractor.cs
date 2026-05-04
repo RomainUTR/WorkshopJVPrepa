d2ba02203c1f45796d6d42d3ffde1f3548b3e776
Lya RENARD <l_renard9@stu-bellecour.fr>
2026-05-04 17:19:29 +0200

    bug fixed

--- Assets/Scripts/PlayerInteractor.cs
+++ Assets/Scripts/PlayerInteractor.cs
@@ -42,7 +42,7 @@ public class PlayerInteractor : MonoBehaviour
         if (Physics.Raycast(ray, out hit, selectionDistance))
         {
             objectSelected = hit.collider.GetComponent<InteractiveObject>();
-            if(objectSelected == null && objectSelected.interactions.Length == 0)
+            if(objectSelected != null && objectSelected.interactions.Length == 0)
                 objectSelected = null;
         }
         else
